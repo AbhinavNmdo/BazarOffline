fde76041c1e27f6f6f4d2f839a974002425baeaf
Abhinav Namdeo <abhaynam22@gmail.com>
2021-09-01 21:30:19 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -9,7 +9,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Welcome<?php 
+        if (!isset($_SESSION['loggedin'])) {
+            echo "Guest";
+        }
+        else {
+            echo $_SESSION['email'];
+        }
+    ?></title>
 </head>
 <style>
 .container {
@@ -39,7 +46,7 @@
         else {
             echo $_SESSION['email'];'
             </h4>
-            <p>Login Successfull</p>';
+            <p>Login Successful</p>';
         }
     ?>
             
