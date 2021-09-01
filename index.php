1d59464529328e2b71fc3213c8a64a7fbaf4c104
Abhinav Namdeo <abhaynam22@gmail.com>
2021-09-01 12:08:44 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php
-    session_start();
 
-    if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != true) {
-        header("location: Login.php");
-        exit;
-    }
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
