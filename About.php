e0bc7cade0d72c502752269951b406c6062ec53e
Abhinav Namdeo <abhaynam22@gmail.com>
2021-09-10 11:24:52 +0530

    Fix

--- About.php
+++ About.php
@@ -26,7 +26,7 @@
       <div class="flex flex-wrap -m-4">
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="photo_2021-07-22_16-24-47.jpg">
+            <img alt="testimonial" class="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="Images/photo_2021-07-22_16-24-47.jpg">
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Mr.Abhishek Namdeo</h2>
             <p class="text-gray-500">Co-Founder</p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
@@ -35,7 +35,7 @@
         </div>
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="photo_2021-07-22_17-20-42.jpg">
+            <img alt="testimonial" class="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="Images/photo_2021-07-22_17-20-42.jpg">
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Mr.Deepak Sehgal</h2>
             <p class="text-gray-500">Co-Founder</p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
@@ -44,7 +44,7 @@
         </div>
         <div class="lg:w-1/3 lg:mb-0 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="photo_2021-07-22_17-23-42.jpg">
+            <img alt="testimonial" class="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="Images/photo_2021-07-22_17-23-42.jpg">
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Abhinav Namdeo</h2>
             <p class="text-gray-500">Developer</p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
